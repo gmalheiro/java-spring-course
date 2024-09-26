82b5da502e70057f5ce6b01d4d8a59c54d75b560
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-09-26 01:03:47 -0300

    feat: add MarioGame class

---
+++ section-19/spring-framework/src/main/java/com/gmalheiro/spring/spring_framework/game/MarioGame.java
@@ -0,0 +1,19 @@
+package com.gmalheiro.spring.spring_framework.game;
+
+public class MarioGame {
+	public void up () {
+		System.out.println("up");
+	}
+	
+	public void down () {
+		System.out.println("down");
+	}
+	
+	public void right () {
+		System.out.println("right");
+	}
+	
+	public void left () {
+		System.out.println("left");
+	}
+}
