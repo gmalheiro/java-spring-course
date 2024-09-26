e3458f43d2a5a80f0d723c80bf2fb4479c7651ca
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-09-26 01:04:04 -0300

    feat: add classes with tightly coupling approach

--- section-19/spring-framework/src/main/java/com/gmalheiro/spring/spring_framework/SpringFrameworkApplication.java
+++ section-19/spring-framework/src/main/java/com/gmalheiro/spring/spring_framework/SpringFrameworkApplication.java
@@ -1,13 +1,19 @@
 package com.gmalheiro.spring.spring_framework;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.gmalheiro.spring.spring_framework.game.GameRunner;
+import com.gmalheiro.spring.spring_framework.game.MarioGame;
 
 @SpringBootApplication
 public class SpringFrameworkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringFrameworkApplication.class, args);
+		//SpringApplication.run(SpringFrameworkApplication.class, args);
+		MarioGame game = new MarioGame();
+		GameRunner runner = new GameRunner(game);
+		runner.run();
 	}
 
 }
