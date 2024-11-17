b3d24e022ad69f86e610119ab8ab32facd32d5af
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-11-17 08:15:09 -0300

    feat: Application is implements CommandLineRunner and add PersonJdbcDao and run method to run findAll method

--- section-03/database-demo/src/main/java/com/spring_database/database_demo/DatabaseDemoApplication.java
+++ section-03/database-demo/src/main/java/com/spring_database/database_demo/DatabaseDemoApplication.java
@@ -1,13 +1,29 @@
 package com.spring_database.database_demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.spring_database.database_demo.databasedemo.jdbc.PersonJdbcDao;
+
 @SpringBootApplication
-public class DatabaseDemoApplication {
+public class DatabaseDemoApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PersonJdbcDao dao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All users -> {}", dao.findAll());
 	}
 
 }
