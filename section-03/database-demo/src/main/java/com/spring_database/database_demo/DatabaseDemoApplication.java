7dca287259e6dc8c897bbb6ee7c0c531227b9384
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-11-17 19:22:23 -0300

    feat: add crud methods

--- section-03/database-demo/src/main/java/com/spring_database/database_demo/DatabaseDemoApplication.java
+++ section-03/database-demo/src/main/java/com/spring_database/database_demo/DatabaseDemoApplication.java
@@ -1,5 +1,7 @@
 package com.spring_database.database_demo;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.spring_database.database_demo.databasedemo.entity.Person;
 import com.spring_database.database_demo.databasedemo.jdbc.PersonJdbcDao;
 
 @SpringBootApplication
@@ -24,6 +27,11 @@ public class DatabaseDemoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		logger.info("All users -> {}", dao.findAll());
+		logger.info("User id 1001 -> {}",dao.findById(1001));
+		logger.info("All users from London -> {}", dao.findByLocation("London"));
+		logger.info("Delete user with id 1001 -> {}", dao.deleteById(1001));
+		logger.info("Insert user with id 1004 -> {}", dao.insertPerson(new Person(1004,"Naruto","Konoha",new Date())));
+		logger.info("Update user with id 1004 -> {}", dao.updatePerson(new Person(1004,"Ayrton","Brasil",new Date())));
 	}
 
 }
--- section-03/database-demo/src/main/java/com/spring_database/database_demo/databasedemo/jdbc/PersonJdbcDao.java
+++ section-03/database-demo/src/main/java/com/spring_database/database_demo/databasedemo/jdbc/PersonJdbcDao.java
@@ -1,5 +1,6 @@
 package com.spring_database.database_demo.databasedemo.jdbc;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +15,33 @@ public class PersonJdbcDao {
 
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-	
+
 	public List<Person> findAll() {
-		return jdbcTemplate.query("select * from person", 
-				new BeanPropertyRowMapper<Person>(Person.class));
+		return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<Person>(Person.class));
 	}
-	
+
+	public Person findById(int id) {
+		return jdbcTemplate.queryForObject("select * from person where id=?",
+				new BeanPropertyRowMapper<Person>(Person.class), id);
+	}
+
+	public int deleteById(int id) {
+		return jdbcTemplate.update("delete from Person where id = ? ", id);
+	}
+
+	public List<Person> findByLocation(String location) {
+		return jdbcTemplate.query("select * from person where location=?",
+				new BeanPropertyRowMapper<Person>(Person.class), location);
+	}
+
+	public int insertPerson(Person person) {
+		return jdbcTemplate.update("insert into person " + " (id,name,location,birth_date) " + " values (?,?,?,?)",
+				person.getId(), person.getName(), person.getLocation(), new Timestamp(person.getBirthDate().getTime()));
+	}
+
+	public int updatePerson(Person person) {
+		return jdbcTemplate.update("update person set name = ? , location = ?, birth_date = ? where id = ?", person.getName(),
+				person.getLocation(), new Timestamp(person.getBirthDate().getTime()),person.getId());
+	}
+
 }
