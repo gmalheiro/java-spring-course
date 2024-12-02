d0d2fbf123f0c24d5a8f3c0c993648fe371282b2
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-02 19:33:47 -0300

    feat: add Course class

---
+++ section-05/app/src/main/java/com/java/jpa/hibernate/app/entity/Course.java
@@ -0,0 +1,44 @@
+package com.java.jpa.hibernate.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	protected Course() {
+		super();
+	}
+
+	public Course(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+}
