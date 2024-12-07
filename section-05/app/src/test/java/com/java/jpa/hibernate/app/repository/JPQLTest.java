627e38ea78012b432534620b24c734409a05ca15
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-07 04:16:54 -0300

    feat: add native queries

--- section-05/app/src/test/java/com/java/jpa/hibernate/app/repository/JPQLTest.java
+++ section-05/app/src/test/java/com/java/jpa/hibernate/app/repository/JPQLTest.java
@@ -2,6 +2,7 @@ package com.java.jpa.hibernate.app.repository;
 
 import java.util.List;
 
+import jakarta.persistence.Query;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ import com.java.jpa.hibernate.app.entity.Course;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
 
 @SpringBootTest(classes = AppApplication.class)
 class JPQLTest {
@@ -29,7 +31,7 @@ class JPQLTest {
 	
 	@Test
 	void findAll_Typed() {
-		TypedQuery<Course> query = em.createQuery("Select c From Course c",Course.class);
+		TypedQuery<Course> query = em.createNamedQuery("query_get_all_courses",Course.class);
 		List<Course> resultList = query.getResultList();
 		logger.info("Select c from Course c typed -> {}", resultList);
 	}
@@ -37,8 +39,40 @@ class JPQLTest {
 	@Test
 	void jpql_Where() {
 		TypedQuery<Course> query = 
-				em.createQuery("Select c From Course c where name like '%steps%' ",Course.class);
+				em.createNamedQuery("query_get_step_courses",Course.class);
 		List<Course> resultList = query.getResultList();
 		logger.info("Select c from Course c  where -> {}", resultList);
 	}
+
+	@Test
+	void native_queries_basic() {
+		Query query = em.createNativeQuery("SELECT * FROM COURSE",Course.class);
+		List<Course> resultList = query.getResultList();
+		logger.info("SELECT * FROM COURSE -> {}", resultList);
+	}
+
+	@Test
+	void native_Queries_Basic_Where() {
+		Query query = em.createNativeQuery("SELECT * FROM COURSE WHERE id = ?",Course.class);
+		query.setParameter(1,1);
+		List result = query.getResultList();
+		logger.info("SELECT FROM COURSE WHERE -> {}", result);
+	}
+
+	@Test
+	void native_Queries_Basic_NamedParameters() {
+		Query query = em.createNativeQuery("SELECT * FROM COURSE WHERE id = :id",Course.class);
+		query.setParameter("id",1);
+		List result = query.getResultList();
+		logger.info("SELECT FROM COURSE WHERE NAMED PARAMETERS -> {}", result);
+	}
+
+	@Test
+	@Transactional
+	void native_Queries_Basic_Update() {
+		Query query = em.createNativeQuery("UPDATE COURSE set update_date=CURRENT_TIMESTAMP()",Course.class);
+		int noOfRowsUpdated = query.executeUpdate();
+		logger.info("noOfRowsUpdated-> {}", noOfRowsUpdated);
+	}
+
 }
