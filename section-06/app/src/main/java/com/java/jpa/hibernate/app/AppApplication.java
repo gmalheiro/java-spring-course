caed921e956a054b5b30d2621082cf80f2816590
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-18 12:39:38 -0300

    feat: add section06 project

---
+++ section-06/app/src/main/java/com/java/jpa/hibernate/app/AppApplication.java
@@ -0,0 +1,34 @@
+package com.java.jpa.hibernate.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.java.jpa.hibernate.app.entity.Course;
+import com.java.jpa.hibernate.app.repository.CourseRepository;
+
+@SpringBootApplication
+public class AppApplication implements CommandLineRunner{
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private CourseRepository repository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Course course = repository.findById(10001L);
+		logger.info("Course 10001 - {} ",course);
+		//repository.deleteById(10002L);
+		repository.save(new Course("Microservices in 100 steps"));
+		repository.playWithEntityManager();
+	}
+
+}
