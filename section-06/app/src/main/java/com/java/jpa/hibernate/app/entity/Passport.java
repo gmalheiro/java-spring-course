a2cf43f3004d9d80301755dd75a9588e5737f4ad
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-18 21:48:49 -0300

    feat: add new entities

---
+++ section-06/app/src/main/java/com/java/jpa/hibernate/app/entity/Passport.java
@@ -0,0 +1,40 @@
+package com.java.jpa.hibernate.app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Passport {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String number;
+
+	public Passport(String number) {
+		super();
+		this.number = number;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Passport [id=" + id + ", number=" + number + "]";
+	}
+}
---
+++ section-06/app/src/main/java/com/java/jpa/hibernate/app/entity/Student.java
@@ -0,0 +1,53 @@
+package com.java.jpa.hibernate.app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@OneToOne
+	private Passport passport;
+	
+	public Student(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	public Passport getPassport() {
+		return passport;
+	}
+
+	public void setPassport(Passport passport) {
+		this.passport = passport;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + "]";
+	}
+	
+}
