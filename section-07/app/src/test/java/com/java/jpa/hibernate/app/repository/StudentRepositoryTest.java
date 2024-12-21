8fa666d2c8bb5bacd3bbc87d08df032a15bf8edb
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-21 01:11:55 -0300

    test: add tests to retrieve course from student and vice-versa

--- section-07/app/src/test/java/com/java/jpa/hibernate/app/repository/StudentRepositoryTest.java
+++ section-07/app/src/test/java/com/java/jpa/hibernate/app/repository/StudentRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.java.jpa.hibernate.app.repository;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.java.jpa.hibernate.app.AppApplication;
+import com.java.jpa.hibernate.app.entity.Course;
 import com.java.jpa.hibernate.app.entity.Passport;
 import com.java.jpa.hibernate.app.entity.Student;
 
@@ -43,6 +46,24 @@ class StudentRepositoryTest {
 		Passport passport = em.find(Passport.class, 3001L);
 		logger.info("passport -> {} ", passport);
 		logger.info("student -> {} ", passport.getStudent().getName());
+	}
+	
+	@Test
+	@Transactional
+	void retrieveCourseFromStudent() {
+		Student student = em.find(Student.class,2001L);
+		List<Course> courses = student.getCourses();
+		logger.info("test-student -> {}", student);
+		logger.info("test-student-courses -> {}", courses);
+	}
+	
+	@Test
+	@Transactional
+	void retrieveStudentFromCourse() {
+		Course course = em.find(Course.class, 10001);
+		List<Student> students = course.getStudents();
+		logger.info("test-course -> {}", course);
+		logger.info("test-student -> {}", students);
 	}
 	
 }
