67134e162a7e7b7ad730a3314fc8bf01b490180a
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-21 00:17:11 -0300

    chore: change JoinTable name

--- section-07/app/src/main/java/com/java/jpa/hibernate/app/entity/Student.java
+++ section-07/app/src/main/java/com/java/jpa/hibernate/app/entity/Student.java
@@ -8,6 +8,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToOne;
 
@@ -25,6 +27,9 @@ public class Student {
 	private Passport passport;
 	
 	@ManyToMany
+	@JoinTable(name = "STUDENT_COURSE",
+	joinColumns = @JoinColumn(name = "STUDENT_ID"),
+	inverseJoinColumns = @JoinColumn(name = "COURSE_ID"))
 	private List<Course> courses;
 	
 	public Student () {}
